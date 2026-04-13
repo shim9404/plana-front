790e74a9496ecc5187b21a40f03e7391c6a53153
hyojin777 <54x9mp@gmail.com>
2026-04-13 17:02:37 +0900

    feat: 마이페이지 레이아웃 구성
    마이페이지 레이아웃 구성
    - 사이드 영역, 콘텐츠 영역 구성
    
    Fixes #DEV-98

--- src/view/pages/Mypage.jsx
+++ src/view/pages/Mypage.jsx
@@ -1,9 +1,39 @@
+import { Layout, Menu } from "antd";
 import PageLayout from "../../components/common/PageLayout";
+
+const { Sider, Content } = Layout;
+
+const layoutStyle = {
+  display: "flex",
+  minHeight: '100vh',
+  margin: '8px 48px'
+};
+
+const sideStyle = {
+  backgroundColor: 'yellow', // 레이아웃 확인용 색상 (개발 완료 후 색상 제거)
+};
+
+const contentStyle = {
+  margin: '8px 0px',
+  padding: 24,
+  minHeight: 280,
+  backgroundColor: 'green', // 레이아웃 확인용 색상 (개발 완료 후 색상 제거)
+}
 
 const Mypage = () => {
   return (
     <PageLayout>
-      <h2>마이 페이지</h2>
+      <Layout style={layoutStyle}>
+        <Sider width={300} style={sideStyle} theme="light">
+          사이드 영역
+          <Menu theme="light">
+            메뉴
+          </Menu>
+        </Sider>
+        <Content style={contentStyle}>
+          콘텐트 영역
+        </Content>
+      </Layout>
     </PageLayout>
   );
 };
