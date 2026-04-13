7b697b07f8a78e8ae3956e0df1a5d943e58ea465
hyojin777 <54x9mp@gmail.com>
2026-04-13 18:30:02 +0900

    feat: 마이페이지 사이드 바 구성
    마이페이지 사이드 바 구성
    - 프로필 박스, 메뉴(회원 정보 수정, 비밀번호 변경) 구성
    
    Fixes #DEV-97

--- src/view/pages/Mypage.jsx
+++ src/view/pages/Mypage.jsx
@@ -1,5 +1,10 @@
-import { Layout, Menu } from "antd";
+import { KeyOutlined, ProfileOutlined } from "@ant-design/icons";
 import PageLayout from "../../components/common/PageLayout";
+import { Layout, Menu } from "antd";
+import { useState } from "react";
+import '../../styles/mypage.css';
+
+import img1 from '../../styles/image1.PNG'; // 테스트용_이미지
 
 const { Sider, Content } = Layout;
 
@@ -7,10 +12,6 @@ const layoutStyle = {
   display: "flex",
   minHeight: '100vh',
   margin: '8px 48px'
-};
-
-const sideStyle = {
-  backgroundColor: 'yellow', // 레이아웃 확인용 색상 (개발 완료 후 색상 제거)
 };
 
 const contentStyle = {
@@ -21,17 +22,48 @@ const contentStyle = {
 }
 
 const Mypage = () => {
+
+  // 회원 정보 초기값
+  const [memberItem, setMemberItem] = useState({
+    memberId: "M1", email: "a@a.com", name: "홍길동", nickname: "내가제일좋아하는게임캐릭터는커비입니다.", profileImage: "" 
+  }) // 테스트용 임의 값 (Backend 연결)
+
+  // 메뉴(회원 정보 수정(1) / 비밀번호 변경(2) / 회원 탈퇴(3)) 선택 
+  const [selectedMenu, setSelectedMenu] = useState('1');
+
   return (
     <PageLayout>
       <Layout style={layoutStyle}>
-        <Sider width={300} style={sideStyle} theme="light">
-          사이드 영역
-          <Menu theme="light">
-            메뉴
-          </Menu>
+        {/* == 사이드 영역 == */}
+        <Sider width={300} theme="light">
+          {/* 상단 프로필 박스 */}
+          <div className="demo-logo-vertical" />  
+            <div className="profile-box">
+              <img className="profile-image" 
+                src={memberItem.profileImage || img1} /> {/* 테스트용_이미지 */}
+              <div className="profile-name">{memberItem.nickname}</div>
+          </div>
+          {/* 메뉴 */}
+          <Menu 
+            className="custom-menu" theme="light" mode="inline"
+            selectedKeys={selectedMenu ? [selectedMenu] : []}
+            onClick={(e) => setSelectedMenu(e.key)}
+            items={[
+              {
+                key: '1',
+                icon: <ProfileOutlined style={{ fontSize: '25px' }} />,
+                label: '회원 정보 수정'
+              },
+              {
+                key: '2',
+                icon: <KeyOutlined style={{ fontSize: '25px' }} />,
+                label: '비밀번호 변경'
+              }
+            ]}
+          />
         </Sider>
         <Content style={contentStyle}>
-          콘텐트 영역
+          콘텐츠 영역
         </Content>
       </Layout>
     </PageLayout>
