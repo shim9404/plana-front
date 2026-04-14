cb7bc7f22eec832ec642cb63a169bd040dd0c5fc
hyojin777 <54x9mp@gmail.com>
2026-04-14 18:09:27 +0900

    feat: 내 여행 페이지 레이아웃 구성
    내 여행 페이지 레이아웃 구성
    - 사이드바 + 콘텐츠 구성
    
    Fixes #DEV-102

--- src/view/pages/MyTripPage.jsx
+++ src/view/pages/MyTripPage.jsx
@@ -1,9 +1,36 @@
 import PageLayout from "../../components/common/PageLayout";
+import { Layout, Menu } from "antd";
+
+const { Sider, Content } = Layout;
+
+const layoutStyle = {
+  display: "flex",
+  minHeight: '100%'
+};
+
+const contentStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  margin: '24px',
+  padding: '48px',
+  background: '#ffffff',
+  borderRadius: '15px',
+  boxShadow: '0 3px 5px rgba(0,0,0,0.5)',
+  height: 'calc(100vh - 150px)',
+  overflowY: 'auto'
+}
 
 const MyTripPage = () => {
   return (
     <PageLayout>
-      <h2>내 여행 페이지</h2>
+      <Layout style={layoutStyle}>
+        {/* == 사이드 영역 == */}
+        <Sider width={'300px'} theme="light">
+        </Sider>
+        {/* == 콘텐츠 영역 == */}
+        <Content style={contentStyle} >
+        </Content>
+      </Layout>
     </PageLayout>
   );
 };
