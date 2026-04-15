b82bbdb34eacffc235787668d957d5227053befe
hyojin777 <54x9mp@gmail.com>
2026-04-15 12:45:52 +0900

    feat: 내 여행 페이지 콘텐츠(북마크 리스트) 구성
    내 여행 페이지 콘텐츠(북마크 리스트) 구성
    - 콘텐츠 상단 구성(여행 이름, 아이콘 버튼)
    - 북마크 리스트 구성(카드 형태)
    - 북마크 리스트 관련 component 생성
    
    Fixes #DEV-105

--- src/view/pages/MyTripPage.jsx
+++ src/view/pages/MyTripPage.jsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import PageLayout from "../../components/common/PageLayout";
+import { IconButton, TextButton } from "../../components/common/PLA_Buttons";
 import { Layout } from "antd";
 import { CompassOutlined, FormOutlined } from "@ant-design/icons";
+import { Download, FilePenLine, MapPinned, Trash2 } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import '../../styles/myTripPage.css';
-import { TextButton } from "../../components/common/PLA_Buttons";
-import { MapPinned, Trash2 } from "lucide-react";
+import BookmarkComponent from "../../components/myTripPage/BookmarkComponent";
 
 const { Sider, Content } = Layout;
 
@@ -50,6 +51,29 @@ const MyTripPage = () => {
   const [trashPlanList, setTrashPlanList] = useState(
     tripItem.filter((item) => item.status === "INACTIVE"),
   );
+  // 북마크 목록(전부) 초기값
+  const [bookmarkList, setBookmarkList] = useState([
+    {bookmarkId: "BM1", areaId: "A1", areaCategory: "FD6", // 음식점
+      title: "맛있는 초밥집", address: "대구 수성구 달구벌대로489안길 40-1", link: "http://naver.com", telephon: "0000-000-0000", bookmarkType: "YELLOW"},
+    {bookmarkId: "BM2", areaId: "A1", areaCategory: "CE7",  // 카페
+      title: "성심당 본점 랄랄랄랄랄랄랄랄랄랄", address: "대전광역시 중구 대종로480번길 15", link: "http://naver.com", telephon: "0000-000-0000", bookmarkType: "RED"},
+    {bookmarkId: "BM3", areaId: "A32", areaCategory: "AT4", // 관광명소
+      title: "미륵원지", address: "대전광역시 동구 냉천로152번길 80 (마산동)", link: "http://naver.com", telephon: "0000-000-0000",bookmarkType: "GREEN"},
+    {bookmarkId: "BM4", areaId: "A33", areaCategory: "etc", // 기타
+      title: "예시1-기타타탙타ㅏ타타타타타타타탙ㅌ타ㅏ타타타탙", address: "대전광역시 동구 냉천로152번길 80 (마산동)", bookmarkType: "ORANGE"},
+    {bookmarkId: "BM5", areaId: "A34", areaCategory: "PK6", // 주차장
+      title: "예시2-주차장", address: "대전광역시 동구 냉천로152번길 80 (마산동)", bookmarkType: "BLUE"},
+    {bookmarkId: "BM6", areaId: "A35", areaCategory: "PO3", // 공공기관
+      title: "예시3-공공기관", address: "대전광역시 동구 냉천로152번길 80 (마산동)", bookmarkType: "PURPLE"},
+    {bookmarkId: "BM7", areaId: "A36", areaCategory: "AT4", // 관광명소
+      title: "예시4-관광명소", address: "대전광역시 동구 냉천로152번길 80 (마산동)", bookmarkType: "PURPLE"},
+    {bookmarkId: "BM8", areaId: "A37", areaCategory: "SC4", // 학교
+      title: "예시5-학교", address: "대전광역시 동구 냉천로152번길 80 (마산동)", bookmarkType: "PURPLE"},
+    {bookmarkId: "BM9", areaId: "A38", areaCategory: "AD5", // 숙박
+      title: "예시6-숙박", address: "대전광역시 동구 냉천로152번길 80 (마산동)", bookmarkType: "PURPLE"}
+  ]);
+
+
 
   // ==========
   // 메뉴 - 여행 목록 선택
@@ -132,13 +156,47 @@ const MyTripPage = () => {
               </div>
             </TextButton>
           </div>
-
-
-
         </Sider>
         {/* == 콘텐츠 영역 == */}
-        <Content style={contentStyle} >
-        </Content>
+        {selectedTrash === false ? ( // 휴지통 버튼 클릭 여부
+          <Content style={contentStyle}>
+            {tripList.length === 0 ? ( // 여행 목록 없는 경우
+              <div className="content-header__title">
+                😥 여행 계획이 없습니다.
+              </div>
+            ) : (
+              <>
+              {/* 콘텐츠 상단 */}
+              <div className="trip-content-space">
+                <div className="trip-content-header">
+                  <CompassOutlined style={{fontSize: '35px'}} />
+                  <span className="trip-content-header__title">
+                    {tripList.find((trip) => trip.tripId === selectedMenu)?.name}
+                  </span>
+                </div>
+                <div className="trip-content-header_button">
+                  <IconButton type="default" width="50px" height="40px">
+                    <Trash2 size={20} />
+                  </IconButton>
+                  <IconButton type="default" width="50px" height="40px"
+                    onClickEvent={() => navigate("/Plan")}>
+                    <FilePenLine size={20} />
+                  </IconButton>
+                  <IconButton type="default" width="50px" height="40px">
+                    <Download size={20} />
+                  </IconButton>
+                </div>
+              </div>
+              {/* 북마크 카드*/}
+              <BookmarkComponent bookmarkList = {bookmarkList}/>
+              </>
+            )}
+          </Content>
+          ) : (
+            <Content style={contentStyle} >
+            </Content>
+          )
+        }
       </Layout>
     </PageLayout>
   );
