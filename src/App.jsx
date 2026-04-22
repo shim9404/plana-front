76d8ceb8c48c834cfeef576f1c1c1b144a335940
shim yujin <shim9404@gmail.com>
2026-04-22 16:49:46 +0900

    refactor: FE Provider 구조 전반 변경
    - main에 있던 일부 Provider를 App으로 이동
    - Header위치 App으로 이동
    
    Fixes #DEV-147

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
+import { useEffect } from "react";
+import LoginModalComponent from "./components/auth/LoginModalComponent";
+import SignUpModalComponent from "./components/auth/SignUpModalComponent";
 import AppRouter from "./router/AppRouter";
+import HeaderMain from "./view/layouts/HeaderMain";
+import { RegionProvider } from "./hooks/home/RegionContext.jsx";
+import { TripInfoProvider } from "./hooks/TripInfoContext.jsx";
+import { useModal } from "./hooks/ModalProvider.jsx";
+
 
 function App() {
 
+  const {
+    isLoginOpen, closeLoginModal,
+    isSignupOpen, closeSignupModal,
+    openLoginModal
+  } = useModal();
+
+  useEffect(() => {
+    const handleExpired = () => {
+      message.warning('세션이 만료되었습니다. 다시 로그인해 주세요.');
+      openLoginModal();
+    };
+    window.addEventListener('trip-session-expired', handleExpired);
+    return () => window.removeEventListener('trip-session-expired', handleExpired);
+  }, []);
+
   return (
-    <>
-      <AppRouter />
-    </>
+    <RegionProvider>
+      <TripInfoProvider>
+        <LoginModalComponent
+          open={isLoginOpen}
+          onClose={closeLoginModal}
+        />
+        <SignUpModalComponent
+          open={isSignupOpen}
+          onClose={closeSignupModal}
+        />
+        <HeaderMain />
+        <AppRouter />
+      </TripInfoProvider>
+    </RegionProvider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,32 +3,11 @@ import App from "./App.jsx";
 import { App as AntdApp, ConfigProvider, message } from "antd";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute.jsx";
-import HomePage from "./view/pages/HomePage.jsx";
-import HeaderMain from "./view/layouts/HeaderMain.jsx";
 import "./styles/global.css";
 import { GLOBAL_TOKEN } from "./styles/antdDesignTokens.js";
-import { ModalProvider, useModal } from "./hooks/ModalProvider.jsx";
 import { AuthProvider } from "./hooks/AuthContext.jsx";
-import { useEffect } from "react";
-import { RegionProvider } from "./hooks/home/RegionContext.jsx";
-import { TripInfoProvider } from "./hooks/TripInfoContext.jsx";
+import { ModalProvider } from "./hooks/ModalProvider.jsx";
 
-const GlobalEventHandler = () => {
-  const { openLoginModal } = useModal();
-
-  useEffect(() => {
-    console.log('이벤트 리스너 등록');
-    const handleExpired = () => {
-      console.log('이벤트 수신!');
-      message.warning('세션이 만료되었습니다. 다시 로그인해 주세요.');
-      openLoginModal();
-    };
-    window.addEventListener('trip-session-expired', handleExpired);
-    return () => window.removeEventListener('trip-session-expired', handleExpired);
-  }, []);
-
-  return null;
-};
 
 /**
  * 인증 필요 여부에 따른 페이지 분리
@@ -41,23 +20,16 @@ createRoot(document.getElementById("root")).render(
         <AntdApp>
           <AuthProvider>
             <ModalProvider>
-              <RegionProvider>
-                <TripInfoProvider>
-                  <GlobalEventHandler />
-                  <HeaderMain />
-                  <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route
-                      path="/*"
-                      element={
-                        <ProtectedRoute>
-                          <App />
-                        </ProtectedRoute>
-                      }
-                    />
-                  </Routes>
-                </TripInfoProvider>
-              </RegionProvider>
+              <Routes>
+                <Route
+                  path="/*"
+                  element={
+                    <ProtectedRoute>
+                      <App />
+                    </ProtectedRoute>
+                  }
+                />
+              </Routes>
             </ModalProvider>
           </AuthProvider>
         </AntdApp>
